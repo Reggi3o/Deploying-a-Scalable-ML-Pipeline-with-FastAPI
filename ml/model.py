dde5372be0b250b27621b9604fea929a29c11cf5
Reggi3o <ajp48901@gmail.com>
2026-09-14 20:48:33 -0600

    implement ML model in model.py

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +19,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -50,7 +51,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : LogisticRegression
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -59,8 +60,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    return model.predict(X)
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +72,15 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    with open(path, 'rb') as file:
+        model = pickle.load(file)
+        return model
+    
 
 
 def performance_on_categorical_slice(
@@ -107,8 +109,8 @@ def performance_on_categorical_slice(
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-    model : ???
-        Model used for the task.
+    model : LogisticRegression
+        Trained LogisticRegression model.
 
     Returns
     -------
@@ -122,7 +124,8 @@ def performance_on_categorical_slice(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        data[data[column_name] == slice_value], categorical_features=categorical_features, label=label, training=False, encoder=encoder, lb=lb]
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = model.predict(X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
