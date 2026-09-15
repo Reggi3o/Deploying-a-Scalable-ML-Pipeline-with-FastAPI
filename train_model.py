76db111ac2826096da2e151243c3a8266f335c31
Reggi3o <ajp48901@gmail.com>
2026-09-14 21:47:19 -0600

    update train_model.py

--- train_model.py
+++ train_model.py
@@ -89,7 +89,7 @@ for col in cat_features:
             column_name=col,
             slice_value=slicevalue,
             categorical_features=cat_features,
-            label='salary',
+            label="salary",
             encoder=encoder,
             lb=lb,
             model=model
