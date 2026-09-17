c1cee37950632207a84663cbe2e5e59a8253eedc
Reggi3o <ajp48901@gmail.com>
2026-09-17 11:02:14 -0600

    deleted TODO tags from comments

--- main.py
+++ main.py
@@ -26,24 +26,23 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = 'model/encoder.pkl' # path to saved encoder 
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = 'model/model.pkl' # path for the saved model 
 model = load_model(path)
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
+# create a RESTful API using FastAPI
+app = FastAPI()
 
-# TODO: create a GET on the root giving a welcome message
+# create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {'message': 'Hello, from the API!'}
 
 
-# TODO: create a POST on a different path that does model inference
+# create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
@@ -69,6 +68,10 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
+        data,
+        cat_features,
+        training=False,
+        encoder=encoder
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed) # predict the result using data_processed
     return {"result": apply_label(_inference)}
